afa9f1c58eb22c3cd20cce5da88cd583e78aba6a
ev.myshko <ev.myshko@tensor.ru>
2020-03-25 12:51:12 +0300

    Добавлен скрипт отправки сообщения

---
+++ hello_world/producer.py
@@ -0,0 +1,19 @@
+import pika
+
+# Создание соединения
+print('Connecting...')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+print('Connected.')
+
+# Создание очереди
+channel.queue_declare(queue='hello')
+
+# Публикация сообщения
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print('Sent "Hello World!"')
+
+# Закрываем соединение
+print('Closing...')
+connection.close()
+print('Close.')
