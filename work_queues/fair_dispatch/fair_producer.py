9552bf040bbd5cac5eb950d38391384e25120b44
ev.myshko <ev.myshko@tensor.ru>
2020-03-25 16:47:30 +0300

    Разработан producer сообщений

--- work_queues/fair_dispatch/fair_producer.py
+++ work_queues/fair_dispatch/fair_producer.py
@@ -1,16 +1,29 @@
-# Справедливая отправка сообщений. При настройке channel.basic_qos(prefetch_count=1)
-# RabbitMQ не отправляет сообщение consumer'y пока тот не обработает текущее сообщение.
-
 import pika
 import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# Насраиваем справедливую отправку сообщений
-channel.basic_qos(prefetch_count=1)
 channel.queue_declare(queue='task_fair')
 
-message = ' '.join(sys.argv[1:])
-channel.basic_publish(exchange='', routing_key='task_fair', body=message)
-print(f'Sent: {message}')
+messages = ('1 ...........',
+            '2 ...............',
+            '3 ..............',
+            '4 .........',
+            '5 ...................',
+            '6 ............',
+            '7 ....',
+            '8 ...',
+            '9 ..',
+            '10 ..............',
+            '11 ...........',
+            '12 ......................', 
+            '13 ..',
+            '14 ..',
+            '15 ..',
+            '16 ..',
+            '17 ........................')
+
+for message in messages:
+    channel.basic_publish(exchange='', routing_key='task_fair', body=message)
+print(f'Sent done')
