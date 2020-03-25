d3707909cd0e2e8d13788c60916e95867051da8d
ev.myshko <ev.myshko@tensor.ru>
2020-03-25 13:22:48 +0300

    Добавлена проверка на наличие очереди

--- work_queues/task_producer.py
+++ work_queues/task_producer.py
@@ -7,6 +7,8 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
+channel.queue_declare(queue='hello')
+
 message = ' '.join(sys.argv[1:])
 channel.basic_publish(exchange='', routing_key='hello', body=message)
 print(f'Sent: {message}')
